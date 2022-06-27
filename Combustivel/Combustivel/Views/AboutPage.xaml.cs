b137d6000932483002fed4aa9209efcbb9ff11e8
isabela.rosa <isabela.rosa@1204-WEB01>
2022-06-27 10:05:06 -0300

    Adicionar arquivos de projeto.

---
+++ Combustivel/Combustivel/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Combustivel.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Combustivel/Combustivel/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Combustivel.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Combustivel.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
